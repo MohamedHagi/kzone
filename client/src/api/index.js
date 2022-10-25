295c0f0fa0bbb6716fac1d930ada4e7545be5761
Mohamed Hagi <mohamed.2423@hotmail.com>
2022-10-25 08:00:09 -0400

    adding local host

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 
-const API = axios.create({ baseURL: "https://kzone-server.herokuapp.com" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
