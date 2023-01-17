5c4fd88a75765434de12151afeb9a22045708482
Mohamed Hagi <34491028+MohamedHagi@users.noreply.github.com>
2023-01-17 12:04:47 -0500

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://web-production-4430.up.railway.app" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
