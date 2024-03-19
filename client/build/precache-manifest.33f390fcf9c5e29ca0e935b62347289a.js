ee8ee247c3702a1dc35135d58931b1a96dd2e559
Mohamed Hagi <mohamed.2423@hotmail.com>
2024-03-19 13:26:38 -0400

    changes

---
+++ client/build/precache-manifest.33f390fcf9c5e29ca0e935b62347289a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdafade7daa67cf2017b2c05974540cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7b6099b7a820598dea",
+    "url": "/static/css/2.8acc99ea.chunk.css"
+  },
+  {
+    "revision": "eb80b57baeda80943ef1",
+    "url": "/static/css/main.42f184e8.chunk.css"
+  },
+  {
+    "revision": "ef7b6099b7a820598dea",
+    "url": "/static/js/2.22564a0e.chunk.js"
+  },
+  {
+    "revision": "ac1e19c4285ed96eb5d81ff2a60f6037",
+    "url": "/static/js/2.22564a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb80b57baeda80943ef1",
+    "url": "/static/js/main.85f5afb2.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "8b4fb0ef02d8b6801003a29ea68492ce",
+    "url": "/static/media/K-ZONE.8b4fb0ef.svg"
+  },
+  {
+    "revision": "9a4b43556342ac6d39fa1c24182e5600",
+    "url": "/static/media/logo.9a4b4355.png"
+  },
+  {
+    "revision": "3b73669210629cb23b67f7134d42a53c",
+    "url": "/static/media/quote.3b736692.svg"
+  }
+]);
